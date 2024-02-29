f36958ce59427122f8645edb4d9ff121c64d9347
kyle <kp3crt@bolton.ac.uk>
2024-02-29 14:58:48 +0000

    Started the main menu making it loop with a while true statement and added in displaying some data from the CPU for testing 22 lines of code written

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
 import psutil
-print(psutil.cpu_count())
-print("cpu cores")
-print(psutil.cpu_stats())
-print("cpu stats")
+while True:
+    print ("Welcome to Brenda's Buggy Byte Barn\n")
+# the menu which will drive the entire programme
+# numbers are being used to make the menu easier to use with multiple items
+# implement error handling if letters or decimals are entered
+    print("Press 1 to see cpu cores")
+    print("Press 2 to see cpu times")
+    print("Press 0 to exit")
+    choice = int(input())
+    if choice < 0:
+        print("Not an option try again")
+    elif choice > 2:
+        print("Not an option try again")
+    elif choice == 1:
+        print("cpu cores")
+        print(psutil.cpu_count())
+    elif choice == 2:
+        print("cpu times")
+        print(psutil.cpu_times())
+    elif choice ==0:
+        break
+    
